471cef62f6f36c8c60ef5fe65a98c5536fbfd543
K. Yurov <kirillisokay@icloud.com>
2023-07-31 00:49:51 +0300

    ????

---
+++ src/app/components/reusable/map-modal.tsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { Box, Modal } from '@mui/material';
+
+// const style = {
+//     position: 'absolute' as 'absolute',
+//     top: '50%',
+//     left: '50%',
+//     transform: 'translate(-50%, -50%)',
+//     width: 400,
+//     bgcolor: 'background.paper',
+//     border: '2px solid #000',
+//     boxShadow: 24,
+//     p: 4,
+//   };
+
+// const MapModal = ({ frame, open, close }) => {
+//     return (
+//         <Modal
+//             open={open}
+//             onClose={close}
+//             aria-labelledby="modal-modal-title"
+//             aria-describedby="modal-modal-description"
+//         >
+//             <Box sx={style}>
+//                 {frame}
+//             </Box>
+//         </Modal>
+//     );
+// };
+
+// export default MapModal;
